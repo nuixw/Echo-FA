6f6b3c027117548ff204aa009e90ad9753c2f901
Nicow <nwnicow@gmail.com>
2024-11-21 03:10:27 +0100

    Delete useContent.ts

--- hooks/useContent.ts
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { useMDXComponent } from "next-contentlayer/hooks"
-
-export function useContent(content: string) {
-  const MDXComponent = useMDXComponent(content)
-  return <MDXComponent />
-}
